98236ffc37d96277b8ec12cefe9b1c81030c7fba
Kacper Kaliński <kacper.kalinski@miquido.com>
2021-12-17 17:33:14 +0100

    Add always and noop overloads

--- Sources/MQ/Utilities/Noop.swift
+++ Sources/MQ/Utilities/Noop.swift
@@ -5,7 +5,18 @@
 /// Does nothing and has empty implementation.
 ///
 /// - Note: Empty methods are striped out in release builds.
-public func noop() {}
+@inlinable
+public func noop() { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing
+/// or no operation..
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+@inlinable
+public func noop() async { /* noop */ }
 
 /// Convenient placeholder for a function that does nothing.
 ///
@@ -15,11 +26,36 @@ public func noop() {}
 /// - Note: Empty methods are striped out in release builds.
 ///
 /// - Parameter _: placeholder
+@inlinable
 public func noop<A1>(
 	_: A1
-) {
-	noop()
-}
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameter _: placeholder
+@inlinable
+public func noop<A1>(
+  _: inout A1
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameter _: placeholder
+@inlinable
+public func noop<A1>(
+  _: A1
+) async { /* noop */ }
 
 /// Convenient placeholder for a function that does nothing.
 ///
@@ -31,12 +67,27 @@ public func noop<A1>(
 /// - Parameters:
 ///   - _: placeholder
 ///   - _: placeholder
+@inlinable
 public func noop<A1, A2>(
 	_: A1,
 	_: A2
-) {
-	noop()
-}
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameters:
+///   - _: placeholder
+///   - _: placeholder
+@inlinable
+public func noop<A1, A2>(
+  _: A1,
+  _: A2
+) async { /* noop */ }
 
 /// Convenient placeholder for a function that does nothing.
 ///
@@ -49,13 +100,30 @@ public func noop<A1, A2>(
 ///   - _: placeholder
 ///   - _: placeholder
 ///   - _: placeholder
+@inlinable
 public func noop<A1, A2, A3>(
 	_: A1,
 	_: A2,
 	_: A3
-) {
-	noop()
-}
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameters:
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+@inlinable
+public func noop<A1, A2, A3>(
+  _: A1,
+  _: A2,
+  _: A3
+) async { /* noop */ }
 
 /// Convenient placeholder for a function that does nothing.
 ///
@@ -69,14 +137,33 @@ public func noop<A1, A2, A3>(
 ///   - _: placeholder
 ///   - _: placeholder
 ///   - _: placeholder
+@inlinable
 public func noop<A1, A2, A3, A4>(
 	_: A1,
 	_: A2,
 	_: A3,
 	_: A4
-) {
-	noop()
-}
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameters:
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+@inlinable
+public func noop<A1, A2, A3, A4>(
+  _: A1,
+  _: A2,
+  _: A3,
+  _: A4
+) async { /* noop */ }
 
 /// Convenient placeholder for a function that does nothing.
 ///
@@ -91,15 +178,36 @@ public func noop<A1, A2, A3, A4>(
 ///   - _: placeholder
 ///   - _: placeholder
 ///   - _: placeholder
+@inlinable
 public func noop<A1, A2, A3, A4, A5>(
 	_: A1,
 	_: A2,
 	_: A3,
 	_: A4,
 	_: A5
-) {
-	noop()
-}
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameters:
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+@inlinable
+public func noop<A1, A2, A3, A4, A5>(
+  _: A1,
+  _: A2,
+  _: A3,
+  _: A4,
+  _: A5
+) async { /* noop */ }
 
 /// Convenient placeholder for a function that does nothing.
 ///
@@ -115,6 +223,7 @@ public func noop<A1, A2, A3, A4, A5>(
 ///   - _: placeholder
 ///   - _: placeholder
 ///   - _: placeholder
+@inlinable
 public func noop<A1, A2, A3, A4, A5, A6>(
 	_: A1,
 	_: A2,
@@ -122,9 +231,31 @@ public func noop<A1, A2, A3, A4, A5, A6>(
 	_: A4,
 	_: A5,
 	_: A6
-) {
-	noop()
-}
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameters:
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+@inlinable
+public func noop<A1, A2, A3, A4, A5, A6>(
+  _: A1,
+  _: A2,
+  _: A3,
+  _: A4,
+  _: A5,
+  _: A6
+) async { /* noop */ }
 
 /// Convenient placeholder for a function that does nothing.
 ///
@@ -141,6 +272,7 @@ public func noop<A1, A2, A3, A4, A5, A6>(
 ///   - _: placeholder
 ///   - _: placeholder
 ///   - _: placeholder
+@inlinable
 public func noop<A1, A2, A3, A4, A5, A6, A7>(
 	_: A1,
 	_: A2,
@@ -149,9 +281,33 @@ public func noop<A1, A2, A3, A4, A5, A6, A7>(
 	_: A5,
 	_: A6,
 	_: A7
-) {
-	noop()
-}
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameters:
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+@inlinable
+public func noop<A1, A2, A3, A4, A5, A6, A7>(
+  _: A1,
+  _: A2,
+  _: A3,
+  _: A4,
+  _: A5,
+  _: A6,
+  _: A7
+) async { /* noop */ }
 
 /// Convenient placeholder for a function that does nothing.
 ///
@@ -169,6 +325,7 @@ public func noop<A1, A2, A3, A4, A5, A6, A7>(
 ///   - _: placeholder
 ///   - _: placeholder
 ///   - _: placeholder
+@inlinable
 public func noop<A1, A2, A3, A4, A5, A6, A7, A8>(
 	_: A1,
 	_: A2,
@@ -178,6 +335,32 @@ public func noop<A1, A2, A3, A4, A5, A6, A7, A8>(
 	_: A6,
 	_: A7,
 	_: A8
-) {
-	noop()
-}
+) { /* noop */ }
+
+/// Convenient placeholder for a function that does nothing.
+///
+/// Convenient function placeholder for no operation.
+/// Does nothing and has empty implementation.
+///
+/// - Note: Empty methods are striped out in release builds.
+///
+/// - Parameters:
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+///   - _: placeholder
+@inlinable
+public func noop<A1, A2, A3, A4, A5, A6, A7, A8>(
+  _: A1,
+  _: A2,
+  _: A3,
+  _: A4,
+  _: A5,
+  _: A6,
+  _: A7,
+  _: A8
+) async { /* noop */ }
