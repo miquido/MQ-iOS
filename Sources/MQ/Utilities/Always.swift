b78ce6c3a48066cbec0e46cd22c863b97620eb03
Kacper Kaliński <kacper.kalinski@miquido.com>
2022-04-05 11:28:56 +0200

    Cleanup function overloads

--- Sources/MQ/Utilities/Always.swift
+++ Sources/MQ/Utilities/Always.swift
@@ -37,36 +37,6 @@ public func alwaysThrowing<V>(
 /// - Parameter value: Value which will be always returned as a result of prepared function.
 /// - Returns: Function returning always the same value.
 @inlinable
-public func alwaysAsync<V>(
-	_ value: @autoclosure @escaping () -> V
-) -> () async -> V {
-	{ value() }
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> () async throws -> V {
-	{ throw error() }
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
 public func always<A1, V>(
 	_ value: @autoclosure @escaping () -> V
 ) -> (A1) -> V {
@@ -135,40 +105,6 @@ public func alwaysThrowing<A1, V>(
 /// - Parameter value: Value which will be always returned as a result of prepared function.
 /// - Returns: Function returning always the same value.
 @inlinable
-public func alwaysAsync<A1, V>(
-	_ value: @autoclosure @escaping () -> V
-) -> (A1) async -> V {
-	{ _ in
-		value()
-	}
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<A1, V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> (A1) async throws -> V {
-	{ _ in
-		throw error()
-	}
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
 public func always<A1, A2, V>(
 	_ value: @autoclosure @escaping () -> V
 ) -> (A1, A2) -> V {
@@ -189,40 +125,6 @@ public func always<A1, A2, V>(
 public func alwaysThrowing<A1, A2, V>(
 	_ error: @autoclosure @escaping () -> Error
 ) -> (A1, A2) throws -> V {
-	{ _, _ in
-		throw error()
-	}
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
-public func alwaysAsync<A1, A2, V>(
-	_ value: @autoclosure @escaping () -> V
-) -> (A1, A2) async -> V {
-	{ _, _ in
-		value()
-	}
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<A1, A2, V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> (A1, A2) async throws -> V {
 	{ _, _ in
 		throw error()
 	}
@@ -271,40 +173,6 @@ public func alwaysThrowing<A1, A2, A3, V>(
 /// - Parameter value: Value which will be always returned as a result of prepared function.
 /// - Returns: Function returning always the same value.
 @inlinable
-public func alwaysAsync<A1, A2, A3, V>(
-	_ value: @autoclosure @escaping () -> V
-) -> (A1, A2, A3) async -> V {
-	{ _, _, _ in
-		value()
-	}
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<A1, A2, A3, V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> (A1, A2, A3) async throws -> V {
-	{ _, _, _ in
-		throw error()
-	}
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
 public func always<A1, A2, A3, A4, V>(
 	_ value: @autoclosure @escaping () -> V
 ) -> (A1, A2, A3, A4) -> V {
@@ -325,40 +193,6 @@ public func always<A1, A2, A3, A4, V>(
 public func alwaysThrowing<A1, A2, A3, A4, V>(
 	_ error: @autoclosure @escaping () -> Error
 ) -> (A1, A2, A3, A4) throws -> V {
-	{ _, _, _, _ in
-		throw error()
-	}
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
-public func alwaysAsync<A1, A2, A3, A4, V>(
-	_ value: @autoclosure @escaping () -> V
-) -> (A1, A2, A3, A4) async -> V {
-	{ _, _, _, _ in
-		value()
-	}
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<A1, A2, A3, A4, V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> (A1, A2, A3, A4) async throws -> V {
 	{ _, _, _, _ in
 		throw error()
 	}
@@ -407,40 +241,6 @@ public func alwaysThrowing<A1, A2, A3, A4, A5, V>(
 /// - Parameter value: Value which will be always returned as a result of prepared function.
 /// - Returns: Function returning always the same value.
 @inlinable
-public func alwaysAsync<A1, A2, A3, A4, A5, V>(
-	_ value: @autoclosure @escaping () -> V
-) -> (A1, A2, A3, A4, A5) async -> V {
-	{ _, _, _, _, _ in
-		value()
-	}
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<A1, A2, A3, A4, A5, V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> (A1, A2, A3, A4, A5) async throws -> V {
-	{ _, _, _, _, _ in
-		throw error()
-	}
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
 public func always<A1, A2, A3, A4, A5, A6, V>(
 	_ value: @autoclosure @escaping () -> V
 ) -> (A1, A2, A3, A4, A5, A6) -> V {
@@ -461,40 +261,6 @@ public func always<A1, A2, A3, A4, A5, A6, V>(
 public func alwaysThrowing<A1, A2, A3, A4, A5, A6, V>(
 	_ error: @autoclosure @escaping () -> Error
 ) -> (A1, A2, A3, A4, A5, A6) throws -> V {
-	{ _, _, _, _, _, _ in
-		throw error()
-	}
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
-public func alwaysAsync<A1, A2, A3, A4, A5, A6, V>(
-	_ value: @autoclosure @escaping () -> V
-) -> (A1, A2, A3, A4, A5, A6) async -> V {
-	{ _, _, _, _, _, _ in
-		value()
-	}
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<A1, A2, A3, A4, A5, A6, V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> (A1, A2, A3, A4, A5, A6) async throws -> V {
 	{ _, _, _, _, _, _ in
 		throw error()
 	}
@@ -543,40 +309,6 @@ public func alwaysThrowing<A1, A2, A3, A4, A5, A6, A7, V>(
 /// - Parameter value: Value which will be always returned as a result of prepared function.
 /// - Returns: Function returning always the same value.
 @inlinable
-public func alwaysAsync<A1, A2, A3, A4, A5, A6, A7, V>(
-	_ value: @autoclosure @escaping () -> V
-) -> (A1, A2, A3, A4, A5, A6, A7) async -> V {
-	{ _, _, _, _, _, _, _ in
-		value()
-	}
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<A1, A2, A3, A4, A5, A6, A7, V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> (A1, A2, A3, A4, A5, A6, A7) async throws -> V {
-	{ _, _, _, _, _, _, _ in
-		throw error()
-	}
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
 public func always<A1, A2, A3, A4, A5, A6, A7, A8, V>(
 	_ value: @autoclosure @escaping () -> V
 ) -> (A1, A2, A3, A4, A5, A6, A7, A8) -> V {
@@ -597,40 +329,6 @@ public func always<A1, A2, A3, A4, A5, A6, A7, A8, V>(
 public func alwaysThrowing<A1, A2, A3, A4, A5, A6, A7, A8, V>(
 	_ error: @autoclosure @escaping () -> Error
 ) -> (A1, A2, A3, A4, A5, A6, A7, A8) throws -> V {
-	{ _, _, _, _, _, _, _, _ in
-		throw error()
-	}
-}
-
-/// Convenience function helper for returning always the same value.
-///
-/// Makes a function returning always the same value.
-/// Due to @autoclosure parameter annotation value is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter value: Value which will be always returned as a result of prepared function.
-/// - Returns: Function returning always the same value.
-@inlinable
-public func alwaysAsync<A1, A2, A3, A4, A5, A6, A7, A8, V>(
-	_ value: @autoclosure @escaping () -> V
-) -> (A1, A2, A3, A4, A5, A6, A7, A8) async -> V {
-	{ _, _, _, _, _, _, _, _ in
-		value()
-	}
-}
-
-/// Convenience function helper for throwing always the same error.
-///
-/// Makes a function throwing always the same error.
-/// Due to @autoclosure parameter annotation error is resolved on demand
-/// and becomes captured by argument closure when used.
-///
-/// - Parameter error: Error which will be always thrown as a result of prepared function.
-/// - Returns: Function throwing always the same error.
-@inlinable
-public func alwaysAsyncThrowing<A1, A2, A3, A4, A5, A6, A7, A8, V>(
-	_ error: @autoclosure @escaping () -> Error
-) -> (A1, A2, A3, A4, A5, A6, A7, A8) async throws -> V {
 	{ _, _, _, _, _, _, _, _ in
 		throw error()
 	}
