146c50ae36f4e708803346dc1a2c0af58df5a822
Piotr Szumiec <47598979+sq9rt-mq@users.noreply.github.com>
2022-05-30 11:39:02 +0200

    Add iOS, Catalyst & tvOS 13 support (#21)

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
 	name: "MQ",
 	platforms: [
-		.iOS(.v14),
+		.iOS(.v13),
 		.macOS(.v11),
-		.macCatalyst(.v14),
-		.tvOS(.v14),
+		.macCatalyst(.v13),
+		.tvOS(.v13),
 		.watchOS(.v7),
 	],
 	products: [
