84f079df18dad11cfad300054e7d985ca9259832
Kacper Kaliński <47140412+KaQuMiQ@users.noreply.github.com>
2022-12-13 08:47:09 +0100

    Add EmptyInstantiable protocol

---
+++ Sources/MQ/Utilities/EmptyInstantiable.swift
@@ -0,0 +1,7 @@
+/// ``EmptyInstantiable`` is a type which
+/// can be instantiated without any additional
+/// arguments making default / zero value instance.
+public protocol EmptyInstantiable {
+	/// Empty instance with default / zero value.
+	static var empty: Self { get }
+}
