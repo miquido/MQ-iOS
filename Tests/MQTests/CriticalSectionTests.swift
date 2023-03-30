7d24c2ef8fc5bfaecea11d70e67c2b4ca9c453f5
Kacper Kaliński <kacper.kalinski@miquido.com>
2023-03-30 16:15:17 +0200

    Add exchange methods for CriticalState

--- Tests/MQTests/CriticalSectionTests.swift
+++ Tests/MQTests/CriticalSectionTests.swift
@@ -20,4 +20,38 @@ final class CriticalSectionTests: XCTestCase {
 			XCTAssertEqual(state.access(\.self), 100_000)
 		}
 	}
+
+	func test_exchange_returnsCurrentAndAssignesNewValue() async throws {
+		let state: CriticalSection<Int> = .init(0)
+		XCTAssertEqual(
+			state.exchange(\.self, with: 42),
+			0
+		)
+		XCTAssertEqual(
+			state.access(\.self),
+			42
+		)
+	}
+
+	func test_conditionalExchange_assignesNewValueWhenMatching() async throws {
+		let state: CriticalSection<Int> = .init(0)
+		XCTAssertTrue(
+			state.exchange(\.self, with: 42, when: 0)
+		)
+		XCTAssertEqual(
+			state.access(\.self),
+			42
+		)
+	}
+
+	func test_conditionalExchange_doesNotAssignNewValueWhenNotMatching() async throws {
+		let state: CriticalSection<Int> = .init(0)
+		XCTAssertFalse(
+			state.exchange(\.self, with: 42, when: 1)
+		)
+		XCTAssertEqual(
+			state.access(\.self),
+			0
+		)
+	}
 }
