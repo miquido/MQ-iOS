23e07cc07c33a7cc65ce5ddb7e1ae5fbd123ca17
Kacper Kaliński <47140412+KaQuMiQ@users.noreply.github.com>
2023-04-06 16:30:06 +0200

    Add basic implementations for Empty

--- Sources/MQ/Utilities/EmptyInstantiable.swift
+++ Sources/MQ/Utilities/EmptyInstantiable.swift
@@ -1,8 +1,56 @@
 /// ``EmptyInstantiable`` is a type which
 /// can be instantiated without any additional
-/// arguments making default / zero value instance.
+/// arguments making empty (default) instance.
 public protocol EmptyInstantiable {
 
-	/// Empty instance with default / zero value.
+	/// Get the empty instance.
 	static var empty: Self { get }
+}
+
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+extension StaticString: EmptyInstantiable {
+
+  public static var empty: Self { .init() }
+}
+
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+extension String: EmptyInstantiable {
+
+  public static var empty: Self { .init() }
+}
+
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+extension Substring: EmptyInstantiable {
+
+  public static var empty: Self { .init() }
+}
+
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+extension Set: EmptyInstantiable {
+
+  public static var empty: Self { .init() }
+}
+
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+extension Array: EmptyInstantiable {
+
+  public static var empty: Self { .init() }
+}
+
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+extension ArraySlice: EmptyInstantiable {
+
+  public static var empty: Self { .init() }
+}
+
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+extension KeyValuePairs: EmptyInstantiable {
+
+  public static var empty: Self { .init() }
+}
+
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+extension Dictionary: EmptyInstantiable {
+
+  public static var empty: Self { .init() }
 }
