6c84f3d9ab4cb403b236f832ff6f7a3bcbb9796b
Kacper Kaliński <kacper.kalinski@miquido.com>
2023-06-16 15:21:15 +0200

    Improve Empty performance

--- Sources/MQ/Utilities/EmptyInstantiable.swift
+++ Sources/MQ/Utilities/EmptyInstantiable.swift
@@ -10,47 +10,55 @@ public protocol EmptyInstantiable {
 // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
 extension StaticString: EmptyInstantiable {
 
+	@inline(__always) @_transparent @_semantics("constant_evaluable")
 	public static var empty: Self { .init() }
 }
 
 // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
 extension String: EmptyInstantiable {
 
+	@inline(__always) @_transparent @_semantics("constant_evaluable")
 	public static var empty: Self { .init() }
 }
 
 // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
 extension Substring: EmptyInstantiable {
 
+	@inline(__always) @_transparent @_semantics("constant_evaluable")
 	public static var empty: Self { .init() }
 }
 
 // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
 extension Set: EmptyInstantiable {
 
+	@inline(__always) @_transparent @_semantics("constant_evaluable")
 	public static var empty: Self { .init() }
 }
 
 // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
 extension Array: EmptyInstantiable {
 
+	@inline(__always) @_transparent @_semantics("constant_evaluable")
 	public static var empty: Self { .init() }
 }
 
 // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
 extension ArraySlice: EmptyInstantiable {
 
+	@inline(__always) @_transparent @_semantics("constant_evaluable")
 	public static var empty: Self { .init() }
 }
 
 // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
 extension KeyValuePairs: EmptyInstantiable {
 
+	@inline(__always) @_transparent @_semantics("constant_evaluable")
 	public static var empty: Self { .init() }
 }
 
 // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
 extension Dictionary: EmptyInstantiable {
 
+	@inline(__always) @_transparent @_semantics("constant_evaluable")
 	public static var empty: Self { .init() }
 }
